88a48d5a53027abc5ac670af0468351778c75eff
Michael Sundvick <msun284@aucklanduni.ac.nz>
2020-03-31 12:40:19 +1300

    Forgot to add dependenies to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = "project47",
     version = '0.0.0',
-    packages = find_packages()
+    packages = find_packages(),
+    install_requires=[
+        'utm',
+        'numpy',
+        'matplotlib',
+        'cartopy',
+        'ortools'
+    ]
+    
 )
