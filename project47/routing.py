1c27cdd43d478d73ca7eb4f623087ea00c3138dd
Michael Sundvick <kynac1@gmail.com>
2020-05-17 18:18:00 +1200

    Rewrite routing code
    Former-commit-id: 9983f22050a79d74ceeba578d16eb01890904d42

--- project47/routing.py
+++ project47/routing.py
@@ -9,24 +9,33 @@ import matplotlib.pyplot as plt
 
 import cartopy.crs as ccrs
 
-class BaseProblem:
+import osmnx as ox
+import networkx as nx
+
+class ORToolsRouting:
     """
     Assumes one depo, set of locations to be served on one day
 
     Attributes
     ----------
-    depo : Location
-        The location the vehicles are based
-    locs : list[Location]
+    locs : int
+        The number of locations. Includes depos.
+    
+    depo : int, optional
+        The location to use as the depo. Ignored if starts and ends are set. Defaults to location 0.
 
+    starts : list[int], optional
+        A list of starting locations for vehicles. Length must be equal to number of vehicles
+    ends : list[int], optional
+        A list of finish locations for vehicles. Length must be equal to number of vehicles
     """
 
-    def __init__(self, depo:Location, locs:list, num_vehicles:int):
-        self.depo = depo
+    def __init__(self, locs:int, num_vehicles=int, depo=0, starts=None, ends=None):
         self.locs = locs
-        self.nodes = locs + [depo]
-        self.depo_node = len(self.locs)
         self.num_vehicles = num_vehicles
+        self.depo = depo
+        self.starts = starts 
+        self.ends = ends
 
         self.manager = None
         self.routing = None
@@ -36,27 +45,44 @@ class BaseProblem:
         self.objective = np.Inf
 
     def create_model(self):
+        """ Creates required ortools objects and stores them in class variables.
+
+        This could potentially be moved into the init function, but I'm concerned we may need to reset these objects,
+        which is easier if this is wrapped separately.
         """
-        """
-        self.manager = pywrapcp.RoutingIndexManager(
-            len(self.nodes),
-            self.num_vehicles,
-            self.depo_node
-        )
+        if self.starts is None and self.ends is None:
+            self.manager = pywrapcp.RoutingIndexManager(
+                self.locs,
+                self.num_vehicles,
+                self.depo
+            )
+        else:
+            assert len(self.starts) == len(self.ends) == self.num_vehicles
+            self.manager = pywrapcp.RoutingIndexManager(
+                self.locs,
+                self.num_vehicles,
+                self.starts,
+                self.ends
+            )
+            
         self.routing = pywrapcp.RoutingModel(self.manager)
     
-    def add_dimension(self, f, slack_max:int, capactity:int, fix_start_cumul_to_zero:bool, name:str):
-        """
-
-        This pattern is reused a lot, so I've rewritten a wrapper to make it less messy
+    def add_dimension(self, distance_matrix:np.array, slack_max:int, capactity:int, fix_start_cumul_to_zero:bool, name:str):
+        """ This pattern is reused a lot, so I've rewritten a wrapper to make it less messy
 
         https://developers.google.com/optimization/reference/python/constraint_solver/pywrapcp#adddimension
 
         Parameters
         ----------
-        f : Function
-            Some function that takes two locations and returns a scalar value
-        Otherwise, see the above link
+        distance_matrix : array-like
+            A 2D np.array of integers that has the distances from all nodes to all other nodes.
+        
+        Otherwise, see the above link. 
+
+        Warning
+        -------
+        The distances must be integer. You can provide non-integral values, but ortools converts them to integer somehow, which will
+        mean results are wrong, especially when the distances are small.
 
         Returns
         -------
@@ -64,14 +90,20 @@ class BaseProblem:
             An ortools dimension object. We can set certain types of objectives on this, mostly span costs (cost for having routes with different lengths)
         callback_index : int
             The index of the callback in the routing model.
-            This is used with self.routing to add some other types of objectives (more classical distance measures)
+            This is used with self.routing to add some other types of objectives (more classical distance measures)        
         """
+
         if self.routing is None:
             self.create_model()
+
         def callback(from_index:int, to_index:int):
+            """ This is the callback passed to the routing solver. It is a closure over the data in the distance matrix.
+            """
             from_node = self.manager.IndexToNode(from_index)
             to_node = self.manager.IndexToNode(to_index)
-            return self.nodes[from_node].distance_to(self.nodes[to_node])
+            return distance_matrix[from_node, to_node]
+
+        # Various ortools specific logic. All of this is required, and must be done in this order.
         transit_callback_index = self.routing.RegisterTransitCallback(callback)
         dimension_name = name
         self.routing.AddDimension(
@@ -82,71 +114,41 @@ class BaseProblem:
             dimension_name,
         )
         dimension = self.routing.GetDimensionOrDie(dimension_name)
+
         return dimension, transit_callback_index
-    
-    def solve(self, tlim=10, log=True):
-        if self.routing is None:
-            self.create_model()
-        self.search_parameters.time_limit.seconds = tlim
-        self.search_parameters.log_search = log
-        sol = self.routing.SolveWithParameters(self.search_parameters)
-        if self.routing.status() == 1:
-            v = sol.ObjectiveValue()
-            if log:
-                print(f"Solved: {self.routing.status()}")
-                print(f"Objective: {v}")
-            if v < self.objective:
-                self._solution = sol
-                self.objective = v
-                return self.get_solution()
 
-        return None
+    def add_time_windows(self, time_matrix:np.array, time_windows:dict, slack_max:int, capacity:int, fix_start_cumul_to_zero:bool, name:str):
+        """ Adds a time windowed constraint
 
-    def get_solution(self, ortools_sol=None):
-        """ Returns the solution in a format independent of ortools.
-        """
-        if ortools_sol is None: ortools_sol = self._solution
-        routes = []
-        vehicles = []
-        if self.routing is not None and ortools_sol is not None:
-            for vehicle_id in range(self.num_vehicles):
-                vehicles.append(self.get_vehicle(vehicle_id))
-                route = []
-                index = self.routing.Start(vehicle_id)
-                while not self.routing.IsEnd(index):
-                    loc = self.nodes[self.manager.IndexToNode(index)]
-                    route.append(loc)
-                    index = self._solution.Value(self.routing.NextVar(index))
-                routes.append(route)
-            self.solution = Solution(routes, vehicles)
-            return self.solution
-        return None
-
-    def get_vehicle(self,n:int):
-        """ Construct an object for the nth vehicle
+        https://developers.google.com/optimization/reference/python/constraint_solver/pywrapcp#adddimension
 
         Parameters
         ----------
-        n
-            Integer from 0..self.num_vehicles
-        """
-        return Vehicle()
-
-class TimeWindows(BaseProblem):
-    """ Extends the problem with time window constraint support
-    """
-    def add_time_windows(self, f, slack_max:int, capacity:int, fix_start_cumul_to_zero:bool, name:str):
-        """ Adds a time windowed constraint
+        time_matrix : array-like
+            A 2D np.array of integers that has the times from all nodes to all other nodes.
+        time_windows : dict
+            A dict that is indexed by the location number, returning an 1d array-like structure with [start_window, end_window]
+        
+        Otherwise, see the above link. 
 
         Warning
         -------
-        Locations must be of the request subtype, so they have the time_window property
+        The times must be integer. You can provide non-integral values, but ortools converts them to integer somehow, which will
+        mean results are wrong, especially when the times are small.
+
+        See Also
+        --------
+        add_dimension
         """
+        if self.routing is None:
+            self.create_model()
+
         def callback(from_index:int, to_index:int):
             from_node = self.manager.IndexToNode(from_index)
             to_node = self.manager.IndexToNode(to_index)
-            return f(self.nodes[from_node], self.nodes[to_node])
+            return time_matrix[from_node, to_node]
 
+        # Same sort of logic as for add_dimension
         transit_callback_index = self.routing.RegisterTransitCallback(callback)
         self.routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
         time = name
@@ -158,63 +160,113 @@ class TimeWindows(BaseProblem):
             time,
         )
         time_dimension = self.routing.GetDimensionOrDie(time)
-        for location_idx, request in enumerate(self.locs):
-            index = self.manager.NodeToIndex(location_idx)
-            time_dimension.CumulVar(index).SetRange(request.time_window[0], request.time_window[1])
-        for i in range(self.num_vehicles):
+
+        # Place time windows on dimension
+        for location_idx in range(self.locs):
+            if location_idx in time_windows:
+                index = self.manager.NodeToIndex(location_idx)
+                time_dimension.CumulVar(index).SetRange(int(time_windows[location_idx][0]), int(time_windows[location_idx][1]))
+
+        # This code was in the example, seems to be for minimizing time. Don't think it's needed here though.
+        '''for i in range(self.num_vehicles):
             self.routing.AddVariableMinimizedByFinalizer(
                 time_dimension.CumulVar(self.routing.Start(i))
             )
             self.routing.AddVariableMinimizedByFinalizer(
                 time_dimension.CumulVar(self.routing.End(i))
-            )
-        return time_dimension, transit_callback_index
+            )'''
 
-class Solution:
+        return time_dimension, transit_callback_index
+    
+    def solve(self, tlim=10, log=True):
+        """ Solves the route. If the solution has a better objective, this saves the solution.
+        """
+
+        if self.routing is None:
+            self.create_model()
+
+        self.search_parameters.time_limit.seconds = tlim
+        self.search_parameters.log_search = log
+
+        sol = self.routing.SolveWithParameters(self.search_parameters)
+
+        if self.routing.status() == 1:
+            v = sol.ObjectiveValue()
+            if log:
+                print(f"Solved: {self.routing.status()} ({SearchStatus[self.routing.status()]})")
+                print(f"Objective: {v}")
+            if v < self.objective:
+                self._solution = sol
+                self.objective = v
+                return self.get_solution()
+
+        return None
+
+    def get_solution(self, ortools_sol=None):
+        """ Returns the solution in a format independent of ortools.
+        """
+
+        if ortools_sol is None: ortools_sol = self._solution
+
+        if self.routing is not None and ortools_sol is not None:
+            routes = []
+            for vehicle_id in range(self.num_vehicles):
+                route = []
+                index = self.routing.Start(vehicle_id)
+                while not self.routing.IsEnd(index):
+                    loc = self.manager.IndexToNode(index)
+                    route.append(loc)
+                    index = self._solution.Value(self.routing.NextVar(index))
+                loc = self.manager.IndexToNode(index)
+                route.append(loc)
+
+                routes.append(route)
+
+            # Construct, save and return solution object
+            self.solution = RoutingSolution(routes)
+            return self.solution
+        
+        # None if routing not completed at all
+        return None
+
+SearchStatus = {
+    0 : "ROUTING_NOT_SOLVED: Problem not solved yet.",
+    1 : "ROUTING_SUCCESS: Problem solved successfully.",
+    2 :	"ROUTING_FAIL: No solution found to the problem.",
+    3 :	"ROUTING_FAIL_TIMEOUT: Time limit reached before finding a solution.",
+    4 :	"ROUTING_INVALID: Model, model parameters, or flags are not valid."
+}
+
+class RoutingSolution:
     """ Independent solution object
 
-    The idea is that this can be produced by the routing model.
-
-    Simulations could then take this initial solution as an input.
+    The idea is that this can be produced by the routing model. This can then be sent to a simulation function.
+    If we try new methods, they should still return routes in this format. This means that we can still use the
+    simulation functions with new methods.
     """
-    def __init__(self, routes:list, vehicles=[], default_vehicle=Vehicle()):
+    
+    def __init__(self, routes:list):
         self.routes = routes
-        self.vehicles = vehicles
-        if not self.vehicles:
-            self.vehicles = [default_vehicle]*len(routes)
-        self.calculate_predictions()
-    
-    def calculate_predictions(self):
-        """ Calculates predicted time and distance between points.
 
-        Could generalise this to other quantities, like capacity, but not sure there's much point right now.
-        """
-        self.predicted_time = [np.zeros(len(route)) for route in self.routes]
-        self.predicted_distance = [np.zeros(len(route)) for route in self.routes]
-        for i,route in enumerate(self.routes):
-            for j,loc in enumerate(route):
-                if j == 0: 
-                    self.predicted_distance[i][j] = 0
-                    self.predicted_time[i][j] = 0
-                else: 
-                    self.predicted_distance[i][j] = self.predicted_distance[i][j-1] + route[j-1].distance_to(loc)
-                    self.predicted_time[i][j] = self.predicted_time[i][j-1] + route[j-1].time_to(loc)
-    
     def __str__(self):
         """ String representation of the solution for printing
         """
-        return "Solution: " + str(self.routes)
+        s = "Routing solution:\n"
+        for route in self.routes:
+            s += "->".join(str(loc) for loc in route) + "\n"
+        return s
 
-    def plot(self):
+    def plot(self,latlons):
         """ Plots the locations and routes on a map
 
         Not sure of the resolution needed, and zooming in can cause issues with images taking too long.
         I'll fix this once we start working with more realistic locations.
         """
+
         ax = plt.axes(projection=ccrs.PlateCarree())
         ax.stock_img()
-        lon = [loc.lon for route in self.routes for loc in route]
-        lat = [loc.lat for route in self.routes for loc in route ]
+        lon = [latlons[loc][0] for route in self.routes for loc in route]
+        lat = [latlons[loc][1] for route in self.routes for loc in route]
 
         ax.scatter(lon, lat, transform=ccrs.Geodetic())
 
@@ -224,8 +276,24 @@ class Solution:
             ax.plot(lon, lat, transform=ccrs.Geodetic(), color=np.random.rand(3))
 
         plt.show()
-            
-        
+    
+    def plot_osm(self, latlons, G):
 
-        
-     
+        nodes = [ox.get_nearest_node(G, latlon) for latlon in latlons]
+
+        osm_routes = []
+        colorlist = []
+        nodecolorlist = []
+        for route in self.routes:
+            color = np.random.rand(3)
+            for i in range(len(route)-1):
+                orig_node = nodes[route[i]]
+                dest_node = nodes[route[i+1]]
+                path =nx.shortest_path(G, orig_node, dest_node, weight='length')
+                osm_routes.append(path)
+                colorlist += [color]*(len(path)-1)
+                nodecolorlist += [color]*2
+
+        return ox.plot_graph_routes(G, osm_routes, route_color=colorlist, orig_dest_node_color=nodecolorlist)
+
+            
--- tests/test_routing copy.py
+++ tests/test_routing copy.py
@@ -1,107 +1,81 @@
-from project47.model import Location, Request
-from project47.routing import TimeWindows, BaseProblem
-from project47.simulation import *
+from project47.routing import ORToolsRouting
 import numpy as np
+import osmnx as ox
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def test_base():
-    np.random.seed(0)
-    locs = [
-        Location(172.7319266,	-43.5111688),
-        Location(172.63913,	-43.5499101),
-        Location(172.6473834,	-43.547876),
-        Location(172.5866324,	-43.5295274),
-        Location(172.622618,	-43.549314),
-        Location(172.6443871,	-43.5194318),
-        Location(172.5677181,	-43.5262727),
-        Location(172.5768752,	-43.5251111),
-        Location(172.6454779,	-43.5308677),
-        Location(172.6100037,	-43.526729),
-        Location(172.6601199,	-43.5334894),
-        Location(172.6593844,	-43.5345317),
-        Location(172.7308814,	-43.5199411),
-        Location(172.6314508,	-43.5280717),
-        Location(172.6215047,	-43.5217694),
-        Location(172.7624258,	-43.5761387),
-        Location(172.6280757,	-43.5220461),
-        Location(172.5798551,	-43.4779506),
-        Location(172.619338,	-43.5055327),
-        Location(172.6468588,	-43.5255113),
-        Location(172.6531345,	-43.5296929),
-        Location(172.5716451,	-43.5163431),
-        Location(172.7070804,	-43.4955977),
-        Location(172.6100037,	-43.526729),
-        Location(172.6344832,	-43.5589434),
-        Location(172.6365762,	-43.5055445),
-        Location(172.6462857,	-43.5170442),
-        Location(172.6293588,	-43.5252464),
-        Location(172.6486995,	-43.5184613),
-        Location(172.6443871,	-43.5194318),
-        Location(172.7122268,	-43.4938774),
-        Location(172.7462063,	-43.5708894),
-        Location(172.5945826,	-43.5132977),
-        Location(172.6514435,	-43.5328785),
-        Location(172.5464753,	-43.5232292),
-        Location(172.5654115,	-43.552104),
-        Location(172.5847619,	-43.5278681),
-        Location(172.5387021,	-43.5141478),
-        Location(172.642015,	-43.5509728),
-        Location(172.6234537,	-43.5208447),
-        Location(172.6609946,	-43.5679401),
-        Location(172.663747,	-43.537706),
-        Location(172.6056399,	-43.5374932),
-        Location(172.6636662,	-43.5427405),
-        Location(172.6303761,	-43.5159515)
+    latlons = [
+        (-43.5111688, 172.7319266),
+        (-43.5499101, 172.63913),
+        (-43.547876 , 172.6473834),
+        (-43.5295274, 172.5866324),
+        (-43.549314 , 172.622618),
+        (-43.5194318, 172.6443871),
+        (-43.5262727, 172.5677181),
+        (-43.5251111, 172.5768752),
+        (-43.5308677, 172.6454779),
+        (-43.526729 , 172.6100037),
+        (-43.5334894, 172.6601199),
+        (-43.5345317, 172.6593844),
+        (-43.5199411, 172.7308814),
+        (-43.5280717, 172.6314508),
+        (-43.5217694, 172.6215047),
+        (-43.5761387, 172.7624258),
+        (-43.5220461, 172.6280757),
+        (-43.4779506, 172.5798551),
+        (-43.5055327, 172.619338),
+        (-43.5255113, 172.6468588),
+        (-43.5296929, 172.6531345),
+        (-43.5163431, 172.5716451),
+        (-43.4955977, 172.7070804),
+        (-43.526729 , 172.6100037),
+        (-43.5589434, 172.6344832),
+        (-43.5055445, 172.6365762),
+        (-43.5170442, 172.6462857),
+        (-43.5252464, 172.6293588),
+        (-43.5184613, 172.6486995),
+        (-43.5194318, 172.6443871),
+        (-43.4938774, 172.7122268),
+        (-43.5708894, 172.7462063),
+        (-43.5132977, 172.5945826),
+        (-43.5328785, 172.6514435),
+        (-43.5232292, 172.5464753),
+        (-43.552104 , 172.5654115),
+        (-43.5278681, 172.5847619),
+        (-43.5141478, 172.5387021),
+        (-43.5509728, 172.642015),
+        (-43.5208447, 172.6234537),
+        (-43.5679401, 172.6609946),
+        (-43.537706 , 172.663747),
+        (-43.5374932, 172.6056399),
+        (-43.5427405, 172.6636662),
+        (-43.5159515, 172.6303761)
     ]
+    locs = len(latlons)
+    place = 'Christchurch, NZ'
+    G = ox.graph_from_place(place, network_type='drive')
+    
+    nodes = [ox.get_nearest_node(G, latlon) for latlon in latlons]
 
-    depo = Location(172.7319266,	-43.5111688)
+    # Build the distance matrix. This should be easily replaced with google maps api's if needed.
+    distance_matrix = np.zeros((locs,locs))
+    for i in range(locs):
+        for j in range(locs):
+            distance_matrix[i,j] = nx.shortest_path_length(G, nodes[i], nodes[j], weight='length')
 
-    prob = BaseProblem(depo, locs, 100)
-    def distance(a,b):
-        return a.distance_to(b)
+    prob = ORToolsRouting(locs, 10) # First location is implicitly the depo
 
-    dist_dim, dist_ind = prob.add_dimension(distance, 0, 300000, True, "Distance")
+    dist_dim, dist_ind = prob.add_dimension(distance_matrix, 0, 50000, True, "Distance")
     #dist_dim.SetGlobalSpanCostCoefficient(100)
     prob.routing.SetArcCostEvaluatorOfAllVehicles(dist_ind)
-
-    solution = None
-    for i in range(3):
-        prob.search_parameters.first_solution_strategy = (
-            14
-        )
-
-        prob.search_parameters.local_search_metaheuristic = (
-            i
-        )
-        new = prob.solve(log=True,tlim=30)
-        if new:
-            solution = new
+    
+    solution = prob.solve()
 
     if solution:
-        print(str(solution))
-        print(static_sim(solution))
-        solution.plot()
-
-def test_time_windows():
-    reqs = [
-        Request(np.random.rand(), np.random.rand(), round(np.random.rand()*10), 10+round(np.random.rand())*10) for _ in range(1)
-    ]
-
-    depo = Location(np.random.rand(), np.random.rand())
-
-    prob = TimeWindows(depo, reqs, 5)
-    def distance(a,b):
-        a.distance_to(b)
-    def time(a,b):
-        a.time_to(b)
-
-    dist_dim,_ = prob.add_dimension(distance, 0, 100000000, True, "Distance")
-    dist_dim.SetGlobalSpanCostCoefficient(100)
-    prob.add_time_windows(time, 0, 23000000, False, "Time")
-
-    solution = prob.solve(log=True)
-    if solution:
-        print(str(solution))
-        solution.plot()
+        print(solution)
+        fig, ax = solution.plot_osm(latlons, G)
+        plt.show()
 
 if __name__ == "__main__":
     test_base()
