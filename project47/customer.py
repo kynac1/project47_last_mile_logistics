f7cfcc1795a3b7b430d1e166372cbf9a0a9f8813
Michael Sundvick <kynac1@gmail.com>
2020-07-29 16:51:56 +1200

    Initial framework for customer object

--- project47/customer.py
+++ project47/customer.py
@@ -1,2 +1,20 @@
+import numpy as np
 
-def default_availability():
+class Customer:
+    def __init__(self, lat, lon, responsiveness, call_responsiveness, presence):
+        self.lat = lat
+        self.lon = lon
+        self.responsiveness = responsiveness
+        self.call_responsiveness = call_responsiveness
+        self.presence = presence
+
+    def visit(self, time):
+        """ Called to determine whether a customer successfully recieves a package.
+
+        Returns True on a successful delivery
+        """
+        for tw in self.presence:
+            if time < tw[1] and time > tw[0]:
+                return np.random.rand() < self.responsiveness
+        return False
+            
