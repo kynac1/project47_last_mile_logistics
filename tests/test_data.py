076c91598865229fb2e5b587278f5062b3d01498
Michael Sundvick <kynac1@gmail.com>
2020-09-14 11:58:13 +1200

    Fix test

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,7 +6,7 @@ def test_get_sample_performance():
     cd = os.path.dirname(os.path.abspath(__file__)).strip("tests") + "data"
     sample_data_csv = os.path.join(cd, "Toll_CHC_November_Sample_Data.csv")
     CHC_data_csv = os.path.join(cd, "christchurch_street.csv")
-    sample_df, CHC_df, CHC_sub, CHC_sub_dict = read_data(sample_data_csv, CHC_data_csv)
+    sample_df, CHC_df, _, CHC_sub, CHC_sub_dict = read_data(sample_data_csv, CHC_data_csv)
 
     lats = []
     lons = []
